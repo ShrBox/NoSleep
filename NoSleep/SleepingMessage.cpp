3a843c6df59717b28ddf12bdb978c516176bf019
ShrBox <shrbox@outlook.com>
2020-08-12 15:07:50 +0800

    upload

---
+++ NoSleep/SleepingMessage.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+void entry()
+{
+	std::cout << "[SleepingMessage] Loaded!\n";
+}
+
+THook(void, "?setSleeping@Player@@UEAAX_N@Z", Player* pl, bool bl)
+{
+	if (bl) {
+		LocateS<WLevel>()->broadcastText(u8"§a[NoSleep] " + pl->getNameTag() + u8"正在尝试睡觉，但他火了");
+		pl->setOnFire(1000);
+	}
+	return original(pl, bl);
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NoSleep
-Sleeping is not allow with this plugin!
+
+Sleeping is not allow with this plugin!  
+Can only run on BedrockX (version>=0711)
+
+在安装了本插件后，一旦玩家尝试去睡觉，就会被点燃  
+只支持BedrockX 0711及以上版本！
+
+## Download
+
+https://wwa.lanzous.com/b07ru67ra  
+密码:i06y
+
+## License
+
+MIT
